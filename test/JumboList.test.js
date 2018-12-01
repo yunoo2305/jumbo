6ff57226840b92f3167559f49000e7ef6b15eefc
Sang-Hyeon Park <twodude@naver.com>
2018-12-01 20:15:34 +0900

    Update JumboList.test.js

--- test/JumboList.test.js
+++ test/JumboList.test.js
@@ -6,29 +6,20 @@ const JumboList = artifacts.require('JumboList')
 
 contract('JumboList', async (accounts) => {
   describe('reviewing()', async()=>{
-    it('should receive store struct., user address, review score as its input parameter', async=>{})
+    it('should receive store struct., user address, review score, deposit as its input parameter', async=>{})
+    it('should be locking deposit')
     it('should affect former voter\'s reward')
     it('should initialization voting amount to zero')
     it('should return review id', async=>{})
   })
-  describe('listing()', async()=>{
-    it('should get each store\'s score', async=>{})
-    it('sorting...', async=>{})
-    it('should return top100 or whatever as return value(list of ids)', async=>{})
+  describe('challenge()', async()=>{
+    it('should receive review id, user address, deposit', async=>{})
+    it('induce voting()')
+    it('should return challenge id')
   })
-  describe('jumbo-ing()', async()=>{
-    it('should receive list of ids', async=>{})
-    it('should return map(ids => integer)', async=>{})
-  })
-  describe('quest()', async()=>{
-    it('should pass validation test', async()=>{}) // voting, transaction from store, etc., ...
-    it('if not already has')
-    it('give item to customer')
-    it('should return pass/non-pass')
-  })
-  describe('food()', async()=>{
-    it('should receive store type, user address, item id as its input parameter', async()=>{})
-    it('minting new item')
-    it('user get item; transfer ownership')
+  describe('voting()', async()=>{
+    it('should receive challenge id, user address, voting amount(+1 or -1)', async()=>{})
+    it('validation test')
+    it('should return total score')
   })
 })
