3f6775ddf688229bed37f271e86df3be80999511
Sang-Hyeon Park <twodude@naver.com>
2018-12-01 16:40:04 +0900

    Update MyJumbo.test.js

--- test/MyJumbo.test.js
+++ test/MyJumbo.test.js
@@ -5,10 +5,17 @@ const should = chai.use(require('chai-bignumber')(BigNumber)).should()
 const MyJumbo = artifacts.require('MyJumbo')
 
 contract('MyJumbo', async (accounts) => {
-  describe('fun()', async()=>{
-    it('should describe', async=>{})
+  describe('feed()', async()=>{
+    it('should receive type of food', async=>{})
+    it('should return level of level of jumbo', async=>{})
   })
-  describe()
-  describe()
+  describe('equip()', async()=>{
+    it('should receive item number', async=>{})
+    it('should return validation of ownership, jumbo\'s state', async=>{})
+  })
+  describe('remove()', async()=>{
+    it('should receive item number', async=>{})
+    it('should return validation of action(rmv), jumbo\'s state', async=>{})
+  })
   describe()
 })
