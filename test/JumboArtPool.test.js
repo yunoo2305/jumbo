dee220ebc58187a871792c34debb8b5e402ee1c2
Sang-Hyeon Park <twodude@naver.com>
2018-12-02 02:16:23 +0900

    Update JumboArtPool.test.js

--- test/JumboArtPool.test.js
+++ test/JumboArtPool.test.js
@@ -5,13 +5,6 @@ const should = chai.use(require('chai-bignumber')(BigNumber)).should()
 const JumboArtPool = artifacts.require('JumboArtPool')
 
 contract('JumboArtPool', async (accounts) => {
-  function registerCandidate(uint8 level, bytes32 hash) external;
-  function vote(uint8 level, bytes32 hash, bool upvote) external;
-  function getCandidate(bytes32 hash) external returns(int256 votes);
-  function getCandidates(uint8 level) external returns(bytes32[] memory ranking);
-  function getJumbo(uint8 level) external returns(bytes32 target);
-
-
   describe('registerCandidate()', async()=>{
     it('should receive level, hash of image as its input parameter', async=>{})
     it('should register new jumbo candidate', async=>{})
